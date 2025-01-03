8261338cfa4fe555566e545057083bc3ce4ad014
Imran <imgha@hotmail.com>
2025-01-03 18:09:06 +0100

    m

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -14,68 +14,38 @@ import UPDATE_COURSE from '../administration/update-course/Update_Course';
 import CREATE_USER from '../administration/user-management/User_Form';
 import { UPDATE_USER } from '../administration/user-management/Update_User';
 
-export const ROUTER = createBrowserRouter([
+export const ROUTER = createBrowserRouter(
+	[
+	  {
+			path: '/',
+			element: <Root />,
+			children: [
+		  { path: '', element: <HOME /> },
+		  { path: 'quiz/:ID', element: <QUIZ /> },
+		  { path: 'login', element: <Login /> },
+		  { path: 'logout', element: <LOGOUT /> },
+		  { path: 'settings', element: <ADMINISTRATION /> },
+		  { path: 'settings/create-course', element: <CREATE_COURSE /> },
+		  { path: 'settings/add-question/:ID', element: <ADD_QUESTION /> },
+		  { path: 'settings/edit/:ID', element: <UPDATE_COURSE /> },
+		  { path: 'settings/create-user', element: <CREATE_USER /> },
+		  { path: 'settings/edit-user/:USERNAME', element: <UPDATE_USER /> },
+		  { path: 'not-found', element: <NOT_FOUND /> },
+		  { path: '*', element: <NOT_FOUND /> },
+			],
+	  },
+	],
 	{
-		path: '/',
-		element: <Root />,
-		children: [
-			{
-				path: '',
-				element: <HOME />
-			},
-			{
-				path: 'quiz/:ID',
-				element: <QUIZ />
-			},
-			{
-				path: '/login',
-				element: <Login />
-			},
-			{
-				path: '/logout',
-				element: <LOGOUT />
-			},
-			{
-				path: '/settings',
-				element: <ADMINISTRATION />
-			},
-			{
-				path: '/settings/create-course',
-				element: <CREATE_COURSE />
-			},
-			{
-				path: '/settings/add-question/:ID',
-				element: <ADD_QUESTION />
-			},
-			{
-				path: '/settings/edit/:ID',
-				element: <UPDATE_COURSE />
-			},
-			{
-				path: '/settings/create-user',
-				element: <CREATE_USER />
-			},
-			{
-				path: '/settings/edit-user/:USERNAME',
-				element: <UPDATE_USER />
-			},
-			{
-				path: '/not-found',
-				element: <NOT_FOUND />
-			}, {
-				path: '*',
-				element: <NOT_FOUND />
-			}
-		]
+	  basename: '/teach-me-ui',
+	  future: {
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionErrorRevalidation: true,
+	  },
 	}
-], {
-	future: {
-		v7_relativeSplatPath: true,
-		v7_fetcherPersist: true,
-		v7_normalizeFormMethod: true,
-		v7_partialHydration: true,
-		v7_skipActionErrorRevalidation: true
-	},
-});
+);
+  
 
 export default ROUTER;
