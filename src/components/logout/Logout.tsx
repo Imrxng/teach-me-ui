69668e422adf999a496cb0070548b2ea1ff913cd
Imran <imgha@hotmail.com>
2025-01-04 18:45:16 +0100

    m

--- src/components/logout/Logout.tsx
+++ src/components/logout/Logout.tsx
@@ -16,7 +16,9 @@ const LOGOUT = () => {
 		if (response.ok) {
 			SET_LOGIN_SESSION(undefined);
 			localStorage.removeItem('persistedRoute');
-			NAVIGATE('/login');	
+			NAVIGATE('/login');
+			document.cookie = 'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+			return;	
 		}
 	};
 	useEffect(() => {
