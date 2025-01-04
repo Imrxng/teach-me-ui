aad5101526898fbf3bff449c32675f1307404cc0
Imran <imgha@hotmail.com>
2025-01-04 18:46:00 +0100

    m

--- src/components/logout/Logout.tsx
+++ src/components/logout/Logout.tsx
@@ -17,7 +17,6 @@ const LOGOUT = () => {
 			SET_LOGIN_SESSION(undefined);
 			localStorage.removeItem('persistedRoute');
 			NAVIGATE('/login');
-			document.cookie = 'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
 			return;	
 		}
 	};
